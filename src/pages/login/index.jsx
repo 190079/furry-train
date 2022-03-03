29f30ddc86e54d04f2460d163446aad5d8696e5f
190079 <190079@softwarica.edu.np>
2022-03-03 21:19:50 +0545

    Front end for Login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import styled from 'styled-components';
 import {Marginer} from '../../components/index'
-import AdminLogo from '../../assets/admin.svg'
+import AdminLogo from '../../assets/admin.svg';
+import {FaUserAlt} from 'react-icons/fa';
+import {RiLockPasswordFill} from 'react-icons/ri'
+
 const LoginMainContainer = styled.nav`
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    @media (max-width: 1200px) {
+        width: 100%;
+    }
     @media (max-width: 991px) {
         width: 100%;
     }
@@ -18,6 +24,9 @@ const LoginInsideContainer = styled.nav`
     flex-direction: column;
     align-items: center;
     width: 75%;
+    @media (max-width: 1200px) {
+        width: 100%;
+    }
     @media (max-width: 991px) {
         width: 100%;
     }
@@ -28,22 +37,38 @@ const LoginFormContainer = styled.nav`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    border: 1px solid white;
     width: 40%;
     height: 30rem;
     &>img {
         width: 100px;
     }
+    &>h3{
+        font-weight: 600;
+    }
 
+    @media (max-width: 1200px) {
+        width: 90%;
+        
+    }
     @media (max-width: 991px) {
         width: 96%;
+        &> nav,button {
+            width: 80%;
+            align-items: center;
+            
+        }
+        &>img{
+            width: 10rem;
+        }
+        
+        
     }
 
 `
 const StyledButton = styled.button`
     border-radius: 8px;
     background-color: var(--button-blue);
-    width: 6rem;
+    width: 8rem;
     height: 2rem;
     text-align: center;
     justify-content: center;
@@ -58,27 +83,99 @@ const StyledButton = styled.button`
     }
 
 `
-
+const InputContainer = styled.nav`
+    display: inline-flex;
+    overflow: hidden;
+    height: 2rem;
+    border-radius: 8px;
+    padding-left: 3px;
+    border: 1px solid var(--dark-bg);
+    &>svg{
+        height: 100%;
+    }
+    &>input{
+        width: 100%;
+    }
+    .darkmode>&{
+        background-color: var(--dark-bg);   
+    }
+    
+    
+`
+const StyledInput = styled.input`
+    text-align: center;
+    height: 100%;
+    align-self: center;
+    outline: none;
+    border: hidden;
+    &:focus{
+            border-bottom: 2px solid var(--link);
+    }
+    &::-webkit-input-placeholder {
+        text-align: center;
+    }
+    .darkmode &{
+        background-color: var(--dark-container);
+        color: var(--dark-text);
+    }
+    
+`
+const isEmpty = (user,pass) => {
+  return (user.length === 0 || pass.length === 0)
+}
 function Login() {
+    const [warning,setWarning] = useState(" ");
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [Loading,setLoading] = useState(false);
+    const [response,setResponse] = useState({});
+    
+    useEffect(() =>{
+        setUsername(username);
+        },[username]);
+    useEffect(() => {
+        setPassword(password);
+        }
+    ,[password])
+
+    if (Loading) {
+        return <nav className="loader" />
+    }
+    if(!Loading){
     return (
         <LoginMainContainer>
             <Marginer direction="vertical" margin="1rem" />
             <LoginInsideContainer >
                 <LoginFormContainer className="round-container">
                 <h3>Login</h3>
-                <Marginer direction="vertical" margin="1rem" />
+                <h4>{warning}</h4>
                 <img src={AdminLogo} alt="Admin Logo" />
-                <Marginer direction="vertical" margin="1rem" />
-                <input className="round-container"/>
-                <Marginer direction="vertical" margin="10px" />
-                <input className="round-container" />
-                <Marginer direction="vertical" margin="10px" />
-                <StyledButton>Log in</StyledButton>
+                <Marginer direction="vertical" margin="10px"  />
+                <InputContainer>
+                    <FaUserAlt />
+                    <StyledInput type="text" id="username" placeholder="Username"  autoFocus required onChange={(user)=> {
+                        setUsername(user.target.value);
+                    }}
+                    />
+                </InputContainer>
+                <Marginer direction="vertical" margin="5px" />
+                <InputContainer>
+                    <RiLockPasswordFill />
+                    <StyledInput type="password" placeholder="Password"  required  onChange={(password)=> {
+                        setPassword(password.target.value);
+                    }}/>
+                </InputContainer>
+                    <Marginer direction="vertical" margin="1rem" required /> 
+                    <StyledButton onClick={() =>{
+                        {isEmpty(username,password) ? setWarning("All the fields are required.") : setWarning("")}
+                        
+                    }}>Log in</StyledButton>
                 </LoginFormContainer>
                 <Marginer direction="vertical" margin="1rem" />
             </LoginInsideContainer>
         </LoginMainContainer>
     )
+    }
 }
 
 export default Login
